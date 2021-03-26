ede5a811af5dddbe38a7b27b83956000bc343872
Stanislaw Kluczewski <stanklu098@student.polsl.pl>
2021-03-26 17:43:28 +0100

    Initial commit
    W co ty teraz grasz, w co grasz ?

---
+++ SnakeGame/SnakeGame.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <allegro5/allegro.h>
+#include <allegro5/allegro_font.h>	
+#include <allegro5/allegro_primitives.h>
+#include <Windows.h>
+#include <iostream>
+#include <cstdlib>
+#include <chrono>
+#include <thread>
+#include <math.h>
+
+
+int main()
+{
+    setlocale(LC_ALL, "polish");
+    std::cout << "Witamy w grze snake!\n" << std::endl;
+
+    system("pause");
+    return 0;
+}
